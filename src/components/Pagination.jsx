ba5cc3f73e46fea2f7e433e001a7822d9e34165d
masiham <masihmooohmmadpoor@gmail.com>
2025-08-27 12:03:57 +0330

    fixed pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,26 +1,41 @@
 "use client";
 import { Pagination } from "@mui/material";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const PaginationComponent = (count) => {
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
-  const current = new URLSearchParams(Array.from(searchParams.entries()));
+  const [page, setPage] = useState(1);
 
-  const paginationHandler = (page) => {
-    current.set("page", page);
+  useEffect(() => {
+    const pageParam = searchParams.get("page");
+    if (pageParam) {
+      setPage(parseInt(pageParam, 10));
+    } else {
+      setPage(1);
+    }
+  }, [searchParams]);
+
+  const paginationHandler = (newPage) => {
+    const current = new URLSearchParams(Array.from(searchParams.entries()));
+    current.set("page", newPage.toString());
     const search = current.toString();
     const query = search ? `?${search}` : "";
     router.push(`${pathname}${query}`, {
       scroll: false,
     });
+    setPage(newPage);
   };
+
   return (
     <div className="flex justify-center items-center col-span-12 md:col-span-9 mb-5 md:col-start-4">
       <Pagination
         count={count.count}
+        page={page}
         onChange={(e, value) => paginationHandler(value)}
+        
         sx={{
           "& .MuiPaginationItem-root": {
             color: "#588157",
