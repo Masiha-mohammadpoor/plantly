0dbf3e787e91a6b8036762b705305014269b43d6
masiham <masihmooohmmadpoor@gmail.com>
2025-09-14 12:22:33 +0330

    home responsive 2

--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -17,7 +17,6 @@ const ProductSlider = ({ products }) => {
 
   const settings = {
     infinite: true,
-    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3500,
@@ -30,15 +29,9 @@ const ProductSlider = ({ products }) => {
       setCurrentSlide(next % productsNumber.length),
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 410,
         settings: {
-          slidesToShow: 4,
-        },
-      },
-      {
-        breakpoint: 880,
-        settings: {
-          slidesToShow: 3,
+          slidesToShow: 1,
         },
       },
       {
@@ -48,9 +41,21 @@ const ProductSlider = ({ products }) => {
         },
       },
       {
-        breakpoint: 410,
+        breakpoint: 880,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1240,
+        settings: {
+          slidesToShow: 5,
         },
       },
     ],
